e9a8facace8ba68410ec04676c27faa7d401360b
BartlomiejStyczen <daltar96@gmail.com>
2017-03-24 22:56:21 +0100

    Organizacja plikow w oddzielnych katalogach

---
+++ Lab3_charakterystyki_czasowe/calkujacy_inercja.m
@@ -0,0 +1,125 @@
+clear all; close all;
+
+% Obiekt calkujacy z inercja I rzedu
+% Odpowiedz skokowa
+% *****************************************
+k = 1; T = 10; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y1 t1] = step(g, 100);
+
+k = 5; T = 10; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y2 t2] = step(g, 100);
+
+k = 10; T = 10; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y3 t3] = step(g, 100);
+
+figure(1)
+plot(t1, y1, 'r', t2, y2, 'g', t3, y3, 'b')
+grid on
+title('Odpowiedz skokowa przy zmiennym wzmocnieniu')
+xlabel('czas [s]')
+ylabel('odpowiedz')
+legend('k = 1','k = 5','k = 10')
+% *****************************************
+k = 1; T = 10; Ti = 0.5;
+g = tf([k], [T*Ti, Ti, 0] );
+[y1 t1] = step(g, 100);
+
+k = 1; T = 10; Ti = 1;
+g = tf([k], [T*Ti, Ti, 0] );
+[y2 t2] = step(g, 100);
+
+k = 1; T = 10; Ti = 1.5;
+g = tf([k], [T*Ti, Ti, 0] );
+[y3 t3] = step(g, 100);
+
+figure(2)
+plot(t1, y1, 'r', t2, y2, 'g', t3, y3, 'b')
+grid on
+title('Odpowiedz skokowa przy zmiennej sta³ej ca³kowania')
+xlabel('czas [s]')
+ylabel('odpowiedz')
+legend('Ti = 0.5','Ti = 1','Ti = 1.5')
+% *****************************************
+k = 1; T = 1; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y1 t1] = step(g, 100);
+
+k = 1; T = 20; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y2 t2] = step(g, 100);
+
+k = 1; T = 60; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y3 t3] = step(g, 100);
+
+figure(3)
+plot(t1, y1, 'r', t2, y2, 'g', t3, y3, 'b')
+grid on
+title('Odpowiedz skokowa przy zmiennej sta³ej czasowej')
+xlabel('czas [s]')
+ylabel('odpowiedz')
+legend('T = 1','T = 7','T = 50')
+% Odpowiedz impulsowa
+% *****************************************
+k = 1; T = 10; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y1 t1] = impulse(g, 100);
+
+k = 5; T = 10; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y2 t2] = impulse(g, 100);
+
+k = 10; T = 10; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y3 t3] = impulse(g, 100);
+
+figure(4)
+plot(t1, y1, 'r', t2, y2, 'g', t3, y3, 'b')
+grid on
+title('Odpowiedz impulsowa przy zmiennym wzmocnieniu')
+xlabel('czas [s]')
+ylabel('odpowiedz')
+legend('k = 1','k = 5','k = 10')
+% *****************************************
+k = 1; T = 10; Ti = 0.5;
+g = tf([k], [T*Ti, Ti, 0] );
+[y1 t1] = impulse(g, 100);
+
+k = 1; T = 10; Ti = 1;
+g = tf([k], [T*Ti, Ti, 0] );
+[y2 t2] = impulse(g, 100);
+
+k = 1; T = 10; Ti = 1.5;
+g = tf([k], [T*Ti, Ti, 0] );
+[y3 t3] = impulse(g, 100);
+
+figure(5)
+plot(t1, y1, 'r', t2, y2, 'g', t3, y3, 'b')
+grid on
+title('Odpowiedz impulsowa przy zmiennym Ti')
+xlabel('czas [s]')
+ylabel('odpowiedz')
+legend('Ti = 0.5','Ti = 1','Ti = 1.5')
+% *****************************************
+k = 1; T = 1; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y1 t1] = impulse(g, 100);
+
+k = 1; T = 7; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y2 t2] = impulse(g, 100);
+
+k = 1; T = 20; Ti = 2;
+g = tf([k], [T*Ti, Ti, 0] );
+[y3 t3] = impulse(g, 100);;
+
+figure(6)
+plot(t1, y1, 'r', t2, y2, 'g', t3, y3, 'b')
+grid on
+title('Odpowiedz impulsowa przy zmiennej sta³ej czasowej')
+xlabel('czas [s]')
+ylabel('odpowiedz')
+legend('T = 5','T = 15','T = 30')
